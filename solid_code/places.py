66e24e29bfa268624e857505c8d9c1373205759a
Mariana Bazhan <77726440+bazhanka@users.noreply.github.com>
2022-07-13 14:33:06 +0300

    Update places.py

--- solid_code/places.py
+++ solid_code/places.py
@@ -1,11 +1,19 @@
 import random
+from abc import ABC, abstractmethod
 
 
-class Place:  # Базовый класс места.
+class Place(ABC):  # Абстрактный класс места.
+
+    @abstractmethod
+    def get_danger(self):  # Обязательный метод, переопределяемый для наследуемых классов.
+        pass
+
+
+class City(Place):  # Наследуемый класс места, инициирующий объект город.
     def __init__(self, name):
         self.name = name
 
-    def get_danger(self):  # Рандомно выбираем опасность.
+    def get_danger(self):  # Рандомно выбираем опасность. Переопределенный метод для городов.
         list_of_dangers = ('Orcs hid in the forest',
                            'Godzilla stands near a skyscraper',
                            'Villains are robbing a bank')
@@ -13,13 +21,19 @@ class Place:  # Базовый класс места.
         print(f'{current_danger} in {self.name}')
 
 
-class Planet(Place):  # Наследуемый класс места, принимающий аргумент "координаты" вместо имени.
+class Planet(Place):  # Наследуемый класс места, принимающий аргумент координаты вместо имени.
     def __init__(self, coordinates: tuple):
-        super(Planet, self).__init__(name=coordinates)
         self.name = coordinates
 
+    def get_danger(self):  # Рандомно выбираем опасность. Переопределенный метод для планет.
+        list_of_dangers = ('Aliens landed',
+                           'Dart Waider attacks',
+                           'Meteor is coming')
+        current_danger = random.choice(list_of_dangers)
+        print(f'{current_danger} in {self.name}')
 
-Kostroma = Place('Kostroma')
-Tokyo = Place('Tokyo')
-London = Place('London')
-Jupiter = Place('(23.09089898, 476.02928286625)')
+
+Kostroma = City('Kostroma')
+Tokyo = City('Tokyo')
+London = City('London')
+Jupiter = Planet((23.09089898, 476.02928286625))
