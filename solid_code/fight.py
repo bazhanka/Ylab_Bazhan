e9bcb215045ff63e9e005511777198c74d63f3af
Mariana Bazhan <77726440+bazhanka@users.noreply.github.com>
2022-07-14 14:34:15 +0300

    Update fight.py

--- solid_code/fight.py
+++ solid_code/fight.py
@@ -1,42 +1,57 @@
 import random
+from abc import ABC, abstractmethod
 
 
-class Fight:  # Основной класс атаки героя. Рукопашный бой.
+class Fight(ABC):  # Абстрактный класс атаки героя.
     def __init__(self):
-        self.attack_list = (self.roundhouse_kick, self.punch)
+        self.attack_list = (self.fight_one, self.fight_two)
 
-    def roundhouse_kick(self):  # Удар ногой.
-        print('Bump')
-
-    def punch(self):  # Удар рукой.
-        print('Ajjjja')
-
-    def attack(self):  # Герой выбирает рандомную атаку из доступных.
+    def attack(self):  # Метод для всех наследуемых классов. Герой выбирает рандомную атаку из доступных.
         my_attack = random.choice(self.attack_list)
         return my_attack()
+
+    @abstractmethod
+    def fight_one(self):  # Первый переопределяемый для наследуемых классов метод боя.
+        pass
+
+    @abstractmethod  # Второй переопределяемый для наследуемых классов метод боя.
+    def fight_two(self):
+        pass
+
+
+class Karate(Fight):  # Наследуемый класс атаки героя. Рукопашный бой.
+    def __init__(self):
+        super(Karate, self).__init__()
+
+    def fight_one(self):  # Удар ногой.
+        print('Bump')
+
+    def fight_two(self):  # Удар рукой.
+        print('Ajjjja')
 
 
 class Gun(Fight):  # Наследуемый класс атаки героя. Огнестрельное оружие.
     def __init__(self):
         super(Gun, self).__init__()
-        self.attack_list = (self.fire_ak47, self.fire_a_gun, self.punch, self.roundhouse_kick)
 
-    def fire_a_gun(self):  # Выстрел из пистолета.
+    def fight_one(self):  # Выстрел из пистолета.
         print('PIU PIU')
 
-    def fire_ak47(self):  # Автоматная очередь.
+    def fight_two(self):  # Автоматная очередь.
         print('Tratatatata')
 
 
-class Superpowers(Fight): # Наследуемый класс атаки героя. Суперсилы.
+class Superpowers(Fight):  # Наследуемый класс атаки героя. Суперсилы.
     def __init__(self):
         super(Superpowers, self).__init__()
-        self.attack_list = (self.punch, self.roundhouse_kick, self.incinerate_with_lasers)
 
-    def incinerate_with_lasers(self): # Стрельба лазером.
+    def fight_one(self):  # Стрельба лазером.
         print('Wzzzuuuup!')
 
+    def fight_two(self):  # Заморозка.
+        print('Freeeeze!')
 
-karate = Fight()
+
+karate = Karate()
 makarov = Gun()
 laser = Superpowers()
