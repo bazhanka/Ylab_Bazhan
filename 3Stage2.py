09b0110c30b007c4b6031959c17b4693e2351d1d
Mariana Bazhan <77726440+bazhanka@users.noreply.github.com>
2022-07-14 18:16:35 +0300

    Update 3Stage2.py

--- 3Stage2.py
+++ 3Stage2.py
@@ -18,8 +18,8 @@ def decorators_decorator(call_count: int, start_sleep_time: int, factor: int, bo
             for count in range(2, call_count + 1):  # Запускаем цикл по количеству call_count.
                 if t < border_sleep_time:  # Если время ожидания меньше конечного, увеличиваем ожидание.
                     t *= factor**count
-                elif t >= border_sleep_time:  # Если время ожидания больше или равно конечному, используем последнее.
-                    t = border_sleep_time
+                    if t >= border_sleep_time:  # Если время ожидания больше или равно конечному, используем последнее.
+                        t = border_sleep_time
                 time.sleep(t)  # Ожидаем.
                 print(f'Запуск номер {count}. Ожидание {t} секунд. Результат декорируемой функции = {result}')
             print('Конец работы')
@@ -30,7 +30,7 @@ def decorators_decorator(call_count: int, start_sleep_time: int, factor: int, bo
     return time_decorator
 
 
-@decorators_decorator(3, 1, 2, 100)
+@decorators_decorator(3, 1, 2, 10)
 def func(n, m):
     return n*m
 
