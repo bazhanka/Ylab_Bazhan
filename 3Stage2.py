88f507dafe9c59ce31e8eb8322f277e7ccd1eab7
Mariana Bazhan <77726440+bazhanka@users.noreply.github.com>
2022-07-14 14:16:21 +0300

    Update 3Stage2.py

--- 3Stage2.py
+++ 3Stage2.py
@@ -1,12 +1,12 @@
 import time
 
 
-def decorators_decorator(call_count: int, start_sleep_time: int, border_sleep_time: int, factor: int):
+def decorators_decorator(call_count: int, start_sleep_time: int, factor: int, border_sleep_time: int):
     # Декоратор декоратора, принимающий на вход аргументы:
     # call_count - количество запусков;
     # start_sleep_time - начальное время повтора;
-    # border_sleep_time - конечное время повтора;
-    # factor - во сколько раз увеличиваем время ожидания.
+    # factor - во сколько раз увеличиваем время ожидания;
+    # border_sleep_time - конечное время повтора.
     def time_decorator(func):  # Декоратор, принимающий на вход функцию.
         def wrapper(*args, **kwargs):
             count = 1
@@ -17,7 +17,7 @@ def decorators_decorator(call_count: int, start_sleep_time: int, border_sleep_ti
             print(f'Запуск номер {count}. Ожидание {t} секунд. Результат декорируемой функции = {result}')
             for count in range(2, call_count + 1):  # Запускаем цикл по количеству call_count.
                 if t < border_sleep_time:  # Если время ожидания меньше конечного, увеличиваем ожидание.
-                    t *= 2 ** factor
+                    t *= factor**count
                 elif t >= border_sleep_time:  # Если время ожидания больше или равно конечному, используем последнее.
                     t = border_sleep_time
                 time.sleep(t)  # Ожидаем.
@@ -30,7 +30,7 @@ def decorators_decorator(call_count: int, start_sleep_time: int, border_sleep_ti
     return time_decorator
 
 
-@decorators_decorator(3, 1, 100, 1)
+@decorators_decorator(3, 1, 2, 100)
 def func(n, m):
     return n*m
 
